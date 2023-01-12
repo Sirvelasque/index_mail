7c86bcbf4b84541bab641d7cf9e29f9017e2ae00
Sirvelasque <velasquezwalter10@yahoo.es>
2023-01-12 00:19:59 -0600

    style table with tailwind

--- interfase/RellFiling/src/App.vue
+++ interfase/RellFiling/src/App.vue
@@ -49,25 +49,28 @@
   </form>
 
   <!-- content table -->
-  <div>
-  <table id="list_table">
+  <div class="flex flex-col md:flex-row px-5 min-w-full max-h-min h-96">
+    <div class="md:h-auto md:max-h-50vh md:w-1/2 md:overflow-y-scroll">
+      <table id="list_table" class="border-2 border-gray-300 bg-white rounded-lg table-fixed">
     <thead>
-      <tr>
-        <th>Subject</th>
-        <th>From</th>
-        <th>To</th>
-      </tr>
+      
+        <tr class="border-b-2 border-gray-300">
+            <th class="w-1/3 px-4 py-2">Subject</th>
+            <th class="w-1/3 px-4 py-2">From</th>
+            <th class="w-1/3 px-4 py-2">To</th>
+        </tr>
     </thead>
     <tbody>
-      <tr v-for="item in result" :key="item.id" @click="selectedContent = item.content">
-        <td>{{ item.subject }}</td>
-        <td>{{ item.from }}</td>
-        <td>{{ item.to }}</td>
-      </tr>
+      <tr v-for="(item, index) in result" :key="item.id" v-bind:class="[index % 2 !== 0 ? 'bg-white' : 'bg-gray-100']" @click="selectedContent = item.content">
+            <td class="w-1/3 md:w-1/2 px-4 py-2">{{ item.subject }}</td>
+            <td class="w-1/3 py-2">{{ item.from }}</td>
+            <td class="w-1/3 px-4 py-2">{{ item.to }}</td>
+        </tr>
     </tbody>
-  </table>
-  <div class="content">
-    <p v-html="selectedContent"></p>
+</table>
+</div>
+  <div class="content md:w-1/2 md:pl-5 pt-10 bg-gray-200 md:max-h-50vh  md:overflow-y-scroll">
+    <p v-html="selectedContent" class="md:w-40%"></p>
   </div>
 </div>
 </section>
